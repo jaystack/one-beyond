ccdfdadecf5fd8979e645dc402180d9cc3a289c0
Pál Tamás <pal.tamas@pptgateway.hu>
2022-06-24 15:57:37 +0200

    remove api prefix

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -10,11 +10,9 @@ import { DrawModule } from './draw/draw.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(DrawModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
   await app.listen(port, () => {
-    Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
+    Logger.log('Listening at http://localhost:' + port);
   });
 }
 
