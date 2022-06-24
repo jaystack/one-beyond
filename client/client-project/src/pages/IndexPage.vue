b8fd03a3d047488bf08dd085320e70bcb9f922e4
Daniel Takacs <daniel.takacs@jaystack.com>
2022-06-24 15:46:22 +0200

    Added word selection modal

--- client/client-project/src/pages/IndexPage.vue
+++ client/client-project/src/pages/IndexPage.vue
@@ -1,5 +1,23 @@
 <template>
   <div>
-   
+    <q-dialog v-model="isChooseDialogShown" persistent transition-show="scale" transition-hide="scale">
+      <div class="q-pa-md q-gutter-sm">
+        <q-btn label="WORD" color="white" text-color="black" size="xl" />
+        <q-btn label="WORD" color="white" text-color="black" size="xl" />
+        <q-btn label="WORD" color="white" text-color="black" size="xl" />
+      </div>
+    </q-dialog>
   </div>
 </template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    return {
+      isChooseDialogShown: ref(false),
+    };
+  },
+}
+</script>
